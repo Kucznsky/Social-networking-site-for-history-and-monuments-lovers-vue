75a5d39ead7f532ef2bd980d6740456e5a6a3dfb
Kucznsky <michal.kuczynski2000@wp.pl>
2024-06-12 23:58:22 +0200

    adjusted app.vue for routing

--- monument-ui-vue/src/App.vue
+++ monument-ui-vue/src/App.vue
@@ -1,23 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar></NavBar>
+  <RouterView></RouterView>
 </template>
 
 <script>
-import HelloWorld from './views/HomePage'
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar'
 
 export default {
   name: 'App',
+  components: {
+    NavBar,
+    RouterView
+  }
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>

07640f7a5bf54f9d90799c8469436d6966573383
Kucznsky <michal.kuczynski2000@wp.pl>
2024-06-13 13:11:41 +0200

    fixed routing

--- monument-ui-vue/src/main.js
+++ monument-ui-vue/src/main.js
@@ -2,7 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).mount('#app')
-
-const app = createApp(App)
-app.use(router)
+createApp(App).use(router).mount('#app')
--- monument-ui-vue/src/views/RegisterLoginPage.vue
+++ monument-ui-vue/src/views/RegisterLoginPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      home
+      register
     </div>
   </template>
   

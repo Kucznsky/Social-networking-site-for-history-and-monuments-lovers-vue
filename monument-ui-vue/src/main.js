bc3bbfb453b9cf38aee93dbb5c9c3ce20fded7ce
Kucznsky <michal.kuczynski2000@wp.pl>
2024-10-05 16:31:10 +0200

    added pinia

--- monument-ui-vue/src/main.js
+++ monument-ui-vue/src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
+app.use(pinia)

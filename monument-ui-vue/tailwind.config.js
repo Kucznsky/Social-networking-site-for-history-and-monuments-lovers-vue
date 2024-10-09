c22b13a6ccaa93c2a6681c90dd7ab2731a12cf80
Kucznsky <michal.kuczynski2000@wp.pl>
2024-10-09 17:06:56 +0200

    added tailwind and daisy ui

---
+++ monument-ui-vue/tailwind.config.js
@@ -0,0 +1,11 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx}',],
+  theme: {
+  extend: {},
+  },
+  plugins: [daisyui],
+}
+
